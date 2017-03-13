6d8cd6bd6993cfe4f4e41f7f14b112dff1be4bca
mattn <mattn.jp@gmail.com>
2017-03-14 01:47:52 +0900

    Support Windows (#1)

--- ccli.go
+++ ccli.go
@@ -2,15 +2,17 @@ package ccli
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/fatih/color"
 	"github.com/urfave/cli"
-	"time"
 )
 
 // NewApp creates a new applications with the given settings
 func NewApp() *cli.App {
 	app := cli.NewApp()
 	app.Compiled = time.Now()
+	app.Writer = color.Output
 	setAppTemplates()
 	return app
 }
